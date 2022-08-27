6cb843a459b7369f3a56851815998153d701e1aa
OritYs <orityair1997@gmail.com>
2022-08-27 14:31:59 +0300

    first commit

---
+++ mm-logic/src/main/java/UserManager.java
@@ -0,0 +1,11 @@
+import com.course.mm.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class UserManager {
+    private User user;
+
+}
